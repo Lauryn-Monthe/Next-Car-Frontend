5c4d5f8397da12b54745c04ad97fa236417843aa
lornamonthe <lauryn.monthe@parcaune.com>
2024-05-08 22:19:44 +0200

    sign up pages

--- src/app/components/customer/customer.component.ts
+++ src/app/components/customer/customer.component.ts
@@ -5,11 +5,12 @@ import { SharedModule } from '../../shared/shared.module';
 import { NgbDateAdapter, NgbDateParserFormatter, NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 import { CustomDateParserFormatter, stringToDate } from '../../shared/customDateParserFormatter';
 import { CustomAdapter } from '../../shared/customAdapter';
+import { RouterEvent, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-customer',
   standalone: true,
-  imports: [ReactiveFormsModule, SharedModule, NgbDatepickerModule],
+  imports: [ReactiveFormsModule, SharedModule, NgbDatepickerModule, RouterLink],
   templateUrl: './customer.component.html',
   styleUrl: './customer.component.scss',
   providers: [
@@ -24,13 +25,14 @@ export class CustomerComponent implements OnInit {
   eyeIcon: string = "fa-eye-slash"
 
   constructor(private fb: FormBuilder, readonly customerService: CustomerService) {
-    this. customerForm = this.fb.group({
+    this.customerForm = this.fb.group({
       lastname: [null, Validators.required],
       firstname: [null, Validators.required],
       birthday: [null, Validators.required],
       email: [null, Validators.required],
       gender: [null, Validators.required],
       phoneNumber: [null, Validators.required],
+      password: [null, Validators.required],
       address: this.fb.group({
         streetAndNumber: [],
         postalCode: [],
@@ -59,7 +61,8 @@ export class CustomerComponent implements OnInit {
       birthday: birthday!.toISOString(),
       gender: value.gender,
       phoneNumber: value.phoneNumber,
-      address: value.address
+      address: value.address,
+      password: value.password
     };
     this.customerService.createCustomer({body: body}).subscribe(value => {
       console.log(value);
--- src/app/components/driver/driver.component.ts
+++ src/app/components/driver/driver.component.ts
@@ -1,12 +1,69 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { SharedModule } from '../../shared/shared.module';
+import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterLink } from '@angular/router';
+import { DriverService } from '../../../../api/services';
+import { stringToDate } from '../../shared/customDateParserFormatter';
 
 @Component({
   selector: 'driver',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, SharedModule, NgbDatepickerModule, RouterLink],
   templateUrl: './driver.component.html',
   styleUrl: './driver.component.scss'
 })
 export class DriverComponent {
+  driverForm: FormGroup;
+  type: string = "password";
+  isText: boolean = false;
+  eyeIcon: string = "fa-eye-slash"
 
+  constructor(private fb: FormBuilder, readonly driverService: DriverService) {
+    this.driverForm = this.fb.group({
+      lastname: [null, Validators.required],
+      firstname: [null, Validators.required],
+      birthday: [null, Validators.required],
+      email: [null, Validators.required],
+      gender: [null, Validators.required],
+      phoneNumber: [null, Validators.required],
+      password: [null, Validators.required],
+      carName: [null, Validators.required],
+      address: this.fb.group({
+        streetAndNumber: [],
+        postalCode: [],
+        country: [null, Validators.required],
+        city: [null, Validators.required]
+      })
+    })
+  }
+  ngOnInit(): void {
+   
+  }
+
+  hideShowPassword() {
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
+    this.isText ? this.type = "text" : this.type = "password";
+  }
+
+  createDriver() {
+    const value = this.driverForm.value;
+    let birthday = stringToDate(value.birthday);
+    const body = {
+      lastname: value.lastname,
+      firstname: value.firstname,
+      email: value.email,
+      birthday: birthday!.toISOString(),
+      gender: value.gender,
+      phoneNumber: value.phoneNumber,
+      address: value.address,
+      password: value.password,
+      carName: value.carName
+    };
+    this.driverService.createDriver({body: body}).subscribe(value => {
+      console.log(value);
+      
+    });
+  }
 }
